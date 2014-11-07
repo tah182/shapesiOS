180b3e9d2ccf5d074a3c504f9ea99f6b9d21af2c
Tah Tatsumoto <tah.tatsumoto@gmail.com>
2014-11-07 15:24:04 -0700

    Colors and ShapeType Enums added

---
+++ Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Colors.swift
+//  ShapesGenerator
+//
+//  Created by Tah Tatsumoto on 11/6/14.
+//  Copyright (c) 2014 Mad Hat Productions. All rights reserved.
+//
+
+import Foundation
+enum Colors : String {
+    case WHITE      = "#FFFFFF"
+    case BLACK      = "#000000"
+    case RED        = "#FF0000"
+    case BLUE       = "#007DFF"
+    case YELLOW     = "#FFFF00"
+    case GREEN      = "#00FF00"
+    case PURPLE     = "#7D00FF"
+    case PINK       = "#FF00FF"
+    case ORANGE     = "#FF7D00"
+    case BROWN      = "#B85A00"
+    
+    var value : String {
+        return self.rawValue
+    }
+}
---
+++ ShapeType.swift
@@ -0,0 +1,66 @@
+//
+//  ShapeType.swift
+//  ShapesGenerator
+//
+//  Created by Tah Tatsumoto on 11/6/14.
+//  Copyright (c) 2014 Mad Hat Productions. All rights reserved.
+//
+enum ShapeType : String {
+    case CIRCLE     = "Circle"
+    case TRIANGLE   = "Triangle"
+    case SQUARE     = "Square"
+    case PENTAGON   = "Pentagon"
+    case HEXAGON    = "Hexagon"
+    case OCTAGON    = "Octagon"
+    case STAR       = "Star"
+    
+    var InnerPoints : Int {
+        get { return self.getInnerPoints() }
+    }
+    
+    var OuterPoints : Int {
+        get { return self.getOuterPoints() }
+    }
+    
+    var name : String {
+        return self.rawValue
+    }
+    
+    func getInnerPoints() -> Int {
+        switch self {
+        case .CIRCLE:
+            fallthrough
+        case .TRIANGLE:
+            fallthrough
+        case .SQUARE:
+            fallthrough
+        case .PENTAGON:
+            fallthrough
+        case .HEXAGON:
+            fallthrough
+        case .OCTAGON:
+            return 0
+        case .STAR:
+            return 5
+        }
+    }
+    
+    func getOuterPoints() -> Int {
+        switch self {
+        case .CIRCLE:
+            return 360
+        case .TRIANGLE:
+            return 3
+        case .SQUARE:
+            return 4
+        case .PENTAGON:
+            return 5
+        case .HEXAGON:
+            return 6
+        case .OCTAGON:
+            return 8
+        case .STAR:
+            return 5
+        }
+    }
+}
